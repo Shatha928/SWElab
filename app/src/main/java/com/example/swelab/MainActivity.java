553fd36955bcf27f66466e2af68fb0cef655f8db
Shatha928 <439200928@student.ksu.edu.sa>
2021-09-23 21:36:47 +0300

    Assignment 2

---
+++ app/src/main/java/com/example/swelab/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.swelab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO: test to do
+
+
+
+    }
+    public void onBtnClick(View view){
+        TextView txtHello = findViewById(R.id.textView);
+        EditText editTxtName = findViewById(R.id.editTxtName);
+        txtHello.setText("Hello "+editTxtName.getText().toString());
+    }
+}
